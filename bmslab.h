631d765197aa18422d1f87b93eba19f4a9257a15
minseok127 <minseok0yoon@gmail.com>
2025-02-14 12:37:53 +0900

    add extern C

--- bmslab.h
+++ bmslab.h
@@ -1,13 +1,20 @@
 #ifndef BMSLAB_H
 #define BMSLAB_H
-
-#include <stddef.h>
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
 typedef struct bmslab bmslab_t;
 
 bmslab_t *bmslab_init(int obj_size, int max_page_count);
-void *bmslab_alloc(bmslab_t *slab);
-void bmslab_free(bmslab_t *slab, void *ptr);
+
 void bmslab_destroy(bmslab_t *slab);
 
+void *bmslab_alloc(bmslab_t *slab);
+
+void bmslab_free(bmslab_t *slab, void *ptr);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 #endif /* BMSLAB_H */
